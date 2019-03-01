ebef8f6d279dca457e22f0c91a0bdfc6fff5ca11
Jason Molloy <jmolloy619@gmail.com>
2019-03-01 10:39:38 -0700

    Sam pushed this. need index.js still

---
+++ server-assets/models/post.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose')
+
+let Schema = mongoose.Schema;
+
+let post = new Schema({
+  //must have either a img or a text can have both
+  description: { type: String },
+  imageURL: { type: String }  //Pretty sure this is how to handle images in our db
+})
+
+//Still need to be sure when a post is deleted, all its comments its related to are also deleted.
+
+
+
+module.exports = mongoose.model('Post', post)
