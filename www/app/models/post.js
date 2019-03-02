1738f0e899da900408fde429f7a57a411acf842c
Ryan Thomas <rrt2335@gmail.com>
2019-03-01 21:33:57 -0700

    Edit

--- www/app/models/post.js
+++ www/app/models/post.js
@@ -21,7 +21,7 @@ export default class Post {
                 <h3 class="card-title">${this.title}</h3>
                 <h5 class="card-text">${this.description}</h5>
                 <button class="mb-3 btn btn-lg btn-light shadow" onclick="app.controllers.postController.viewActivePost('${this._id}')">View Peeve</button><br>
-                <button class="btn btn btn-danger shadow" onclick="app.controllers.postController.postHot('${this._id}')">Hot <i class="fas fa-fire"></i></button><div id="hot-counter"># hot: ${this.postHot}</div><br>
+                <button class="btn btn btn-danger shadow" onclick="app.controllers.postController.postHot('${this._id}')">Hot <i class="fas fa-fire"></i></button><div id="hot-counter"># hot: ${this.postHot}</div>
                 <button class="btn btn btn-primary shadow" onclick="app.controllers.postController.postCool('${this._id}')">Cool <i class="fas fa-snowflake"></i></button><div id="cool-counter"># cool: ${this.postCool}</div>
                 </div>
         </div>
@@ -38,9 +38,9 @@ export default class Post {
                     <button class="btn btn-danger shadow" onclick="app.controllers.postController.hot('${this._id}')"><i class="fas fa-fire"></i></button>
                     <button class="btn btn-primary shadow" onclick="app.controllers.postController.cool('${this._id}')"><i class="fas fa-snowflake"></i></button>
                 </div>
-                <form class="form-inline" onsubmit="app.controllers.postController.createComment(event)">
+                <form class="form-inline px-3" onsubmit="app.controllers.postController.createComment(event)">
                     <input type="text" class="form-control mb-2 mr-sm-2" id="comment-input" name="description" placeholder="Enter comment here...">
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                    <button type="submit" class="btn btn-success mb-2"><i class="fas fa-check"></i></button>
                 </form>
         </div>
         <div>
