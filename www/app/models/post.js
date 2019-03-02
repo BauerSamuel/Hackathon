5b8e13bb6f18df33112ebd7c0748244edc8f2292
Ryan Thomas <rrt2335@gmail.com>
2019-03-01 21:24:36 -0700

    Button changes with icons

--- www/app/models/post.js
+++ www/app/models/post.js
@@ -1,9 +1,11 @@
 export default class Post {
     constructor(data) {
+        console.log(data)
         this._id = data._id
         this.title = data.title
         this.description = data.description
-        this.image = data.image
+        this.image = data.image || '//placehold.it/200x200'
+        console.log(this.image)
         this.comments = data.comments
         this.postHot = data.postHot || 0
         this.postCool = data.postCool || 0
@@ -19,8 +21,8 @@ export default class Post {
                 <h3 class="card-title">${this.title}</h3>
                 <h5 class="card-text">${this.description}</h5>
                 <button class="mb-3 btn btn-lg btn-light shadow" onclick="app.controllers.postController.viewActivePost('${this._id}')">View Peeve</button><br>
-                <button class="btn btn btn-danger shadow" onclick="app.controllers.postController.postHot('${this._id}')">Hot</button><div id="hot-counter"># hot: ${this.postHot}</div><br>
-                <button class="btn btn btn-primary shadow" onclick="app.controllers.postController.postCool('${this._id}')">Cool</button><div id="cool-counter"># cool: ${this.postCool}</div>
+                <button class="btn btn btn-danger shadow" onclick="app.controllers.postController.postHot('${this._id}')">Hot <i class="fas fa-fire"></i></button><div id="hot-counter"># hot: ${this.postHot}</div><br>
+                <button class="btn btn btn-primary shadow" onclick="app.controllers.postController.postCool('${this._id}')">Cool <i class="fas fa-snowflake"></i></button><div id="cool-counter"># cool: ${this.postCool}</div>
                 </div>
         </div>
         `
@@ -33,8 +35,8 @@ export default class Post {
                 <div class="card-body">
                     <h1 class="card-title">${this.title}</h1>
                     <h4 class="card-text">${this.description}</h4>
-                    <button class="btn btn-danger shadow" onclick="app.controllers.postController.hot('${this._id}')">Hot</button>
-                    <button class="btn btn-primary shadow" onclick="app.controllers.postController.cool('${this._id}')">Cool</button>
+                    <button class="btn btn-danger shadow" onclick="app.controllers.postController.hot('${this._id}')"><i class="fas fa-fire"></i></button>
+                    <button class="btn btn-primary shadow" onclick="app.controllers.postController.cool('${this._id}')"><i class="fas fa-snowflake"></i></button>
                 </div>
                 <form class="form-inline" onsubmit="app.controllers.postController.createComment(event)">
                     <input type="text" class="form-control mb-2 mr-sm-2" id="comment-input" name="description" placeholder="Enter comment here...">
